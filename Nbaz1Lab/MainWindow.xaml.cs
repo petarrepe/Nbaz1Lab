717154924df1ab394210078bb16fbd1dfae6b9e8
Petar Stojanović <petar.stojanovic@fer.hr>
2016-10-21 14:51:32 +0200

    added basic look

--- Nbaz1Lab/MainWindow.xaml.cs
+++ Nbaz1Lab/MainWindow.xaml.cs
@@ -2,27 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Data.SqlClient;
+
 
 namespace Nbaz1Lab
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private List<string> menuChoices = new List<string>() { "Search", "Add","Analysis" };
         public MainWindow()
         {
             InitializeComponent();
+            menuListBox.ItemsSource = menuChoices;
+            OpenDatabaseConnection();
+        }
+
+        private void OpenDatabaseConnection()
+        {
+            string ConnectionString = "Network Library=DBMSSOCN;" + "Data Source= 192.168.56.12,5432; Initial Catalog=datadasename;" + "User Id=root; Password=reverse;";
+            SqlConnection connection;
+            connection = new SqlConnection(ConnectionString);
+            //connection.Open();
+        }
+
+        private void menuListBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            menuListBox = sender as System.Windows.Controls.ListBox;
+            string selectedItem = menuListBox.SelectedItem.ToString();
         }
     }
 }
